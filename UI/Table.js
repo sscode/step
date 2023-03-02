94f273af9c0d98f3ca6b35e8f200388011b669ae
Stuart Sim <stuartsim.aus@gmail.com>
2023-03-02 14:17:05 -0800

    table split

--- UI/Table.js
+++ UI/Table.js
@@ -4,25 +4,83 @@ import { GlobalStyles } from '../constants/styles'
 
 function TableOne(data){
 
+    const fullWorkout = data.data.filter(
+        (item) => item.id === 0
+    )
+
+    const body = data.data.filter(
+        (item) => item.id !== 0
+    )
+
+    console.log(body)
+
     const item = ({item}) => (
         <View style={styles.itemRow}>
             <View style={[styles.column, styles.column1]}>
-                <Text style={styles.text}>{item.id}</Text>
-            </View>
-            <View style={[styles.column, styles.column2]}>
                 <Text style={styles.text}>{item.time}</Text>
             </View>
-            <View style={[styles.column, styles.column3]}>
+            <View style={[styles.column, styles.column2]}>
                 <Text style={styles.text}>{item.distance}</Text>
+            </View>
+            <View style={[styles.column, styles.column3]}>
+                <Text style={styles.text}>{item.watts}</Text>
+            </View>
+            <View style={[styles.column, styles.column3]}>
+                <Text style={styles.text}>{item.spm}</Text>
             </View>
         </View>
     )
+
+    const totals = ({item}) => (
+        <View style={styles.itemRow}>
+            <View style={[styles.column, styles.column1]}>
+                <Text style={styles.text}>{item.time}</Text>
+            </View>
+            <View style={[styles.column, styles.column2]}>
+                <Text style={styles.text}>{item.distance}</Text>
+            </View>
+            <View style={[styles.column, styles.column3]}>
+                <Text style={styles.text}>{item.watts}</Text>
+            </View>
+            <View style={[styles.column, styles.column3]}>
+                <Text style={styles.text}>{item.spm}</Text>
+            </View>
+        </View>
+    )
+
+    const headerList = () => (
+        <View style={[styles.itemRow, styles.headerRow]}>
+            <View style={[styles.column, styles.header]}>
+                <Text style={[styles.text, styles.headerCell]}>Time</Text>
+            </View>
+            <View style={[styles.column, styles.header]}>
+                <Text style={[styles.text, styles.headerCell]}>Distance</Text>
+            </View>
+            <View style={[styles.column, styles.header]}>
+                <Text style={[styles.text, styles.headerCell]}>Watts</Text>
+            </View>
+            <View style={[styles.column, styles.header]}>
+                <Text style={[styles.text, styles.headerCell]}>spm</Text>
+            </View>
+        </View>
+    )
+
+
     return (
         <View style={styles.container}>
             <FlatList
-                data={data.data}
+                listKey={0}
+                data={fullWorkout}
                 renderItem={item}
                 keyExtractor={item => item.id}
+                ListHeaderComponent={headerList}
+            />
+            <FlatList
+                listKey={1}
+                data={body}
+                renderItem={item}
+                keyExtractor={item => item.id}
+                style={{marginTop: 12}}
             />
         </View>
     )
@@ -39,9 +97,15 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'flex-start',
         justifyContent: 'flex-start',
+        marginBottom: 4,
+    },
+    headerRow: {
+        
+        marginTop: 8,
+        marginBottom: 8,
     },
     column: {
-        width: '33.33%',
+        width: '25%',
     },
     column1: {
         // backgroundColor: 'blue',
@@ -54,12 +118,13 @@ const styles = StyleSheet.create({
     },
     headerCell: {
         color: 'white',
+        fontWeight: 'bold',
     },
     text: {
         fontSize: 12,
         color: GlobalStyles.colors.white,
         textAlign: 'center',
-        borderWidth: 1,
-        borderColor: 'white',
+        // borderWidth: 1,
+        // borderColor: 'white',
     }
 })
--- util/tableFormatter.js
+++ util/tableFormatter.js
@@ -5,6 +5,12 @@ export default function tableDataFormatter(fullData){
 
     const fullDict = []
 
+    //set headers
+    // const tempDict = {"id": 0}
+    // labels.forEach(label => {
+    //     tempDict[label] = label
+    // });
+    // fullDict.push(tempDict)
     //for each header
     for (let i = 0; i < headers.length; i++){
         const header = headers[i]
@@ -14,7 +20,7 @@ export default function tableDataFormatter(fullData){
             for (let x = 0; x < labels.length; x++) {
                 myDict[labels[x]] = fullData[header][j][x];
             }
-            myDict["id"] = j
+            myDict["id"] = j;
             fullDict.push(myDict)
         }
 
