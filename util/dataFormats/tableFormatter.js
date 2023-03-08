4ab8d77725130ff7efadccd3b754b0beca943459
Stuart Sim <stuartsim.aus@gmail.com>
2023-03-07 17:21:56 -0800

    less post-processing on data

--- util/dataFormats/tableFormatter.js
+++ util/dataFormats/tableFormatter.js
@@ -13,46 +13,23 @@ function wattsOrSplit(str){
 export default function tableDataFormatter(fullData){
 
     const headers = ["tableData"];
-    // const labels = ["time", "distance"]
-    let labels = []
     let id = 0;
 
     const fullDict = []
-
-    if(isLetter(fullData.tableHead[0].charAt(0))){
-        // console.log("True String Header and not values")
-        // console.log("tableHead: ", fullData.tableHead)
-        labels = fullData.tableHead
-        // console.log("labels: ", labels)
-    } else {
-        //see if third value contains : or . or ,
-        const thirdValue = wattsOrSplit(fullData.tableData[0][2])
-        labels = ["time", "distance", thirdValue, "spm"]
-        // console.log("False String Header and instead is #")
-        //read this and add it to fullDict
-        const header = "tableHead"
-        const myDict = {}
-        for (let x = 0; x < labels.length; x++) {
-            myDict[labels[x]] = fullData[header][x];
-        }
-        myDict["id"] = 0;
-        id += 1;
-        fullDict.push(myDict)
-    }
 
     const header = headers[0]
     //for each row
     for(let j = 0; j < fullData[header].length; j++){
         const myDict = {}
-        for (let x = 0; x < labels.length; x++) {
-            myDict[labels[x]] = fullData[header][j][x];
+        for (let x = 0; x < fullData.tableHead.length; x++) {
+            myDict[fullData.tableHead[x]] = fullData[header][j][x];
         }
         myDict["id"] = j + id;
         fullDict.push(myDict)
     }
 
-    labels.push("id")
+    fullData.tableHead.push("id")
 
     // console.log("tableDataFormatter: ", fullDict)
-    return [fullDict, labels]
+    return [fullDict, fullData.tableHead]
 }
